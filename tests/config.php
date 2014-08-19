f271426a6fe7c4ac8e996e2411a4434bc44136ec
Jared King <j@jaredtking.com>
2014-08-18 22:22:42 -0500

    added mysql support to tests

---
+++ tests/config.php
@@ -0,0 +1,28 @@
+<?php
+
+return  [
+  'site' => [
+    'salt' => 'replacewithrandomstring',
+  ],
+  'modules' => [
+    'middleware' => [
+      'auth'
+    ],
+    'all' => [
+    	'auth',
+      'statistics'
+    ]
+  ],
+  'database' => [
+    'type' => 'mysql',
+    'user' => 'travis',
+    'password' => '',
+    'host' => '127.0.0.1',
+    'name' => 'mydb',
+  ],
+  'sessions' => [
+    'enabled' => true,
+    'adapter' => 'database',
+    'lifetime' => 86400
+  ],
+];
