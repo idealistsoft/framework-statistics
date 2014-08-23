880bf4c384089c42f861bd5c5e1fe90aefc0c913
Jared King <j@jaredtking.com>
2014-08-23 14:34:04 -0500

    use phinx for db migrations

---
+++ src/app/statistics/migrations/20140823142449_statistic.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Statistic extends AbstractMigration
+{
+    /**
+     * Change Method.
+     */
+    public function change()
+    {
+        $table = $this->table( 'Statistics', [ 'id' => false, 'primary_key' => [ 'metric', 'day' ] ] );
+        $table->addColumn( 'metric', 'string', [ 'length' => 100 ] )
+              ->addColumn( 'day', 'string', [ 'length' => 10 ] )
+              ->addColumn( 'val', 'text' )
+              ->addColumn( 'ts', 'integer');
+    }
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+    
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
