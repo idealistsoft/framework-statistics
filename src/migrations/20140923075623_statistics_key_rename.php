6013639246d33af858b97b7902875e31a5e06b27
Jared King <j@jaredtking.com>
2014-09-23 07:58:57 -0500

    added migration for new key format

---
+++ src/migrations/20140923075623_statistics_key_rename.php
@@ -0,0 +1,29 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class StatisticsKeyRename extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $rows = $this->fetchAll('SELECT * FROM Statistics');
+
+        foreach ($rows as $row) {
+            $metric = $row['metric'];
+            $metric = str_replace('-', '_', $metric);
+
+            $this->execute('UPDATE Statistics SET metric = "' . $metric . '" WHERE id = ' . $row['id']);
+        }
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
